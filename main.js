f667cb3dacb4405bcdcc2b3b799751c32d631ee6
ahyeong2533 <ahyeong2533@gmail.com>
2019-01-09 21:32:16 +0900

    url 디코딩 구현
    파일명 없으면 해당 경로 base62로 디코딩

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var url = require("url");
 var fs = require('fs');
 var mysql = require('mysql');
 var qs = require('querystring');
@@ -11,31 +12,61 @@ var sqlconnect = mysql.createConnection({
     database : 'urlshort'
   });
 
+console.log('mysql connected');
+sqlconnect.connect();
 
 var app = http.createServer(function(request,response){
-    var url = request.url;
+    var requrl = request.url;
 
     if(request.url == '/'){
-      url = '/index.html';
-
+      requrl = '/index.html';
+      
+      // 입력받은 url 파싱
       var body = '';
       request.on('data', function(data){
           body = body + data;
       });
-  
+    
       request.on('end', function(){
         var post = qs.parse(body);
-        var urls = post.url;
-        console.debug(urls);
       });
+
+      // 신규 url이면 DB 등록
     }
-    if(request.url == '/favicon.ico'){
+    else if(request.url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
+    else{
+      try {
+        fs.accessSync(__dirname + requrl);
+      }
+      catch(err){
+        // 해당 파일명이 없을경우 DB에서 key검사
+        var path = requrl.substring(1);
+        var key = base62.decode(path);
+        console.log(key);
+
+        connection.query('SELECT * FROM urltable WHERE id=?', key, function (error, results, fields) {
+          if (error) {
+              console.log(error);
+          }
+          console.log(results);
+        });
+        //response.writeHead(404);
+        //response.end();
+        return;
+      }
+    }
+
+    // 해당되는 파일로 응답
+    console.log('response');
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(fs.readFileSync(__dirname + requrl));
  
 });
 app.listen(3000);
+
+console.log('mysql disconnected');
+sqlconnect.end();
